b5accd9550fcb61cda1bf2b8ac233efeb0df2083
rlaehdals <rkdlem48@gmail.com>
2022-02-12 16:46:23 +0900

    feat SSL/HTTPS

--- src/main/java/com/example/securitybasic/SecuritybasicApplication.java
+++ src/main/java/com/example/securitybasic/SecuritybasicApplication.java
@@ -1,7 +1,13 @@
 package com.example.securitybasic;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,4 +25,31 @@ public class SecuritybasicApplication {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public ServletWebServerFactory serverFactory(){
+        TomcatServletWebServerFactory tomcatServlet = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcatServlet.addAdditionalTomcatConnectors(httpConnector());
+
+        return tomcatServlet;
+
+    }
+
+    private Connector httpConnector(){
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
